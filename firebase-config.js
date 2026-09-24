ead002fc2d0713dbdcce50fa433ce1f9ca0d7648
topjob-dot <topjobsseekers@gmail.com>
2026-09-24 08:29:29 +0100

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+
+export const firebaseConfig = {
+  apiKey:"AIzaSyB0CwidiZ0qNupPRQkQInquJUwClLIizDw ",
+  authDomain: "topjobs-76cd5.firebaseapp.com",
+  projectId: "topjobs-76cd5",
+  appId: "1:393878014015:web:6cf2714618d2d6488dd08a"
+};
